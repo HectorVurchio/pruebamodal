675bbee369531899c5f7b3ba47651ae387c51f85
Hector Vurchio <hectorvurchio@gmail.com>
2023-04-17 12:21:08 -0400

    fixing Flash Message

--- src/components/FlashMessage.vue
+++ src/components/FlashMessage.vue
@@ -1,8 +1,16 @@
 <template>
-  <div id="yellowMessage" v-if="this.$store.state.yellowMessage">
+  <div
+    class="alert alert-success"
+    role="alert"
+    v-if="this.$store.state.yellowMessage"
+  >
     {{ this.$store.state.yellowMessage }}
   </div>
-  <div id="redMessage" v-if="this.$store.state.redMessage">
+  <div
+    class="alert alert-danger"
+    role="alert"
+    v-if="this.$store.state.redMessage"
+  >
     {{ this.$store.state.redMessage }}
   </div>
 </template>
@@ -11,37 +19,4 @@ export default {
   name: "FlashMessage",
 };
 </script>
-<style scoped>
-@keyframes yellowfade {
-  from {
-    background: yellow;
-  }
-  to {
-    background: transparent;
-  }
-}
-#yellowMessage {
-  animation-name: yellowfade;
-  animation-duration: 6s;
-  height: 60px;
-  font-size: 40px;
-  color: #786868;
-  box-shadow: 0px 0px 30px 0px #878737;
-}
-@keyframes redfade {
-  from {
-    background: red;
-  }
-  to {
-    background: transparent;
-  }
-}
-#redMessage {
-  animation-name: redfade;
-  animation-duration: 6s;
-  height: 60px;
-  font-size: 40px;
-  color: yellow;
-  box-shadow: 0px 0px 30px 0px #483434;
-}
-</style>
+<style scoped></style>
